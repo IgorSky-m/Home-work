7754e638fec3e662554340801e0eaf6a9fa76a82
Igor Skachko <scarydisco@gmail.com>
2020-03-23 12:24:29 +0300

    Записал решение posNeg (отвлекли по работе и пропустил случайно)

--- codingbat/warmup_1/src/codingbat/warmup_1/PosNeg.java
+++ codingbat/warmup_1/src/codingbat/warmup_1/PosNeg.java
@@ -1,12 +1,13 @@
 package codingbat.warmup_1;
 
 public class PosNeg {
-    public boolean posNeg (int a, int b) {
-
-        return true;
+    public boolean posNeg (int a, int b, boolean neg) {
+        if (neg == true) {
+            return a <0 && b <0 ? true : false;
+        } else {
+            return (a<0 && b>0) || (b<0 && a>0) ? true : false;
+        }
     }
 }
 
-
-// - 1 , -2
 
